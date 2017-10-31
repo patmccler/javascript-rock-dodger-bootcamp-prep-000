da9dc95c0c4a1d60bd5739ca4f25674d599b19f1
Patrick McClernan <patmccler@gmail.com>
2017-10-31 00:22:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ function endGame() {
   {
     GAME.removeChild(ROCKS[i])
   }
-  window.removeEventListener('keydown', moveDodger(e))
+  window.removeEventListener('keydown', moveDodger
 }
 
 function moveDodger(e) {
