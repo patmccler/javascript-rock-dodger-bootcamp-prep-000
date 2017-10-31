8089d6634344b107f8650010c40ae90908ca54f3
Patrick McClernan <patmccler@gmail.com>
2017-10-31 00:29:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -146,10 +146,14 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if(e.which == LEFT_ARROW) {
+   if(e.which === LEFT_ARROW) {
+     e.preventDefault()
+     e.stopPropagation()
      moveDodgerLeft()
    }
    else if (e.which == RIGHT_ARROW) {
+     e.preventDefault()
+     e.stopPropagation()
      moveDodgerRight()
    }
 }
