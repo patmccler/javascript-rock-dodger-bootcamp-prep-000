13ec38b5ea7a5f4226e2d2e8a3b824b53b0309ac
Patrick McClernan <patmccler@gmail.com>
2017-10-31 16:21:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function checkCollision(rock) {
 
     if ( (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge)
       || (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge)
-      || (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)
+      || (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge) )
       {
       //alert("OOPS")
       //rock.style.color = "red"
